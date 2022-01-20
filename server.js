df3a375ce8c02952efc35420e76ebaa9e60bc1ae
Roberts Gutmanis <r.gutmanis9751@gmail.com>
2022-01-20 20:16:29 +0200

    finsished

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv')
 const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors')
 const app = express();
+app.use(cors());
 app.use(express.json());
 dotenv.config()
 
@@ -17,6 +20,18 @@ const db = mysql.createConnection({
     database: 'sql11463787'
 })
 
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.send("error0")
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        if(err) return res.send("error1")
+        req.user = user
+        next()
+    })
+}
+
 const getAllData = () => {
     let sql = `
     SELECT emails.email, passwords.password, numbers.number, usernames.username FROM main
@@ -28,16 +43,20 @@ const getAllData = () => {
          result.forEach(function(data,i){
              allEmails[i] = data.email
              allUsernames[i] = data.username
-             allNumbers[i] = data.number
+             allNumbers[i] = data.number+""
          })
-         console.log(allEmails, allUsernames, allNumbers)
      })
 }
 
 db.connect(function(err) {
     if (err) throw err;
     getAllData()
+    console.log("Connected")
 });
+
+app.get("/", (req, res)=>{
+    res.send("home route")
+})
 
 
 app.post('/register', async function(req, res) {
@@ -46,28 +65,34 @@ app.post('/register', async function(req, res) {
         const hash = await bcrypt.hash(password, 10);
 
         const numToString = number + ""
+
         if(numToString[0]!=2){
-            throw new Error("Incorrect number")
+            throw new Error("IN")
+        }
+        if(allEmails.includes(email)){
+            throw new Error("WE")
+        }
+        if(allUsernames.includes(username)){
+            throw new Error("WU")
         }
 
-        if(allEmails.includes(email)||
-        allUsernames.includes(username)||
-        allNumbers.includes(number)){
-            throw new Error("In use")
+        if(allNumbers.includes(number)){
+            throw new Error("WN")
         }
+
 
         let sql = `INSERT INTO emails (email) VALUES ('${email}');`
         await db.query(sql, (err, result)=>{
-
+            if (err) throw err;
             let sql = `INSERT INTO numbers (number) VALUES (${number});`
             db.query(sql, (err, result)=>{
-            
+                if (err) throw err;
                 let sql = `INSERT INTO passwords (password) VALUES ('${hash}');`
                 db.query(sql, (err, result)=>{
-            
+                    if (err) throw err;
                     let sql = `INSERT INTO usernames (username) VALUES ('${username}');`
                     db.query(sql, (err, result)=>{
-                        
+                        if (err) throw err;
                         let sql = `INSERT INTO main (email_id, number_id, password_id, username_id) VALUES (
                                      (SELECT id FROM emails WHERE email='${email}'),
                                      (SELECT id FROM numbers WHERE number=${number}),
@@ -75,7 +100,7 @@ app.post('/register', async function(req, res) {
                                      (SELECT id FROM usernames WHERE username='${username}')
                                  )`
                         db.query(sql, (err, result)=>{
-                            if (err) res.status(400).json({"message": "There was an error"});
+                            if (err) throw err;
                             getAllData()
                             res.status(200).json({
                                 "status": "done"
@@ -86,8 +111,8 @@ app.post('/register', async function(req, res) {
             })
         })
     }catch(e){
-        console.log(e)
-        res.send("Something broke!")
+        const Error = e.message
+        res.status(400).json({"status": false, "message": Error})
     }
 })
 
@@ -96,8 +121,9 @@ app.post("/login", async function(req, res) {
         const {email, password} = req.body
         let sql = `SELECT id, email FROM emails WHERE email='${email}'`
         db.query(sql, (err, result)=>{
+            if (err) throw err;
             if(result==""){
-                res.send("No such account exists")
+                res.json({"status": false})
             }else{
             const user_id = result[0].id
             let sql = `SELECT password FROM passwords WHERE id=${user_id}`
@@ -105,25 +131,38 @@ app.post("/login", async function(req, res) {
                 const user_hash = result1[0].password
                 const validPass = await bcrypt.compare(password, user_hash)
                 if(validPass){
-                    res.status(200).json(true)
+                    const user = {email: result[0].email}
+                    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+
+                    res.status(200).json({
+                        "status": true,
+                        "token": accessToken
+                    })
+
                 }else{
-                    res.status(400).json(false)
+                    res.status(400).json({
+                        "status": false
+                    })
                 }
             })}
         })
     }catch(e){
-        console.log(e)
         res.send("Something went wrong")
     }
 })
 
-
-app.get('/:username', function (req, res) {
-    let sql = `SELECT * FROM emails`
+app.get('/account', authenticateToken, function (req, res) {
+    let sql = `SELECT id, email FROM emails WHERE email='${req.user.email}'`
     db.query(sql, function (err, result) {
-        res.send(result)
+        const user_id = result[0].id
+        let sql = `SELECT emails.email, numbers.number, usernames.username FROM main
+        LEFT JOIN emails ON emails.id=${user_id}
+        LEFT JOIN numbers on numbers.id=${user_id}
+        LEFT JOIN usernames ON usernames.id=${user_id}`
+        db.query(sql, (err, result1)=>{
+            res.send(result1[0])
+        })
     })
 })
-
 
 app.listen(process.env.PORT || 3000)
